764612a262f4f1ef1aff42cddb9648b7eb820cbc
Christopher Thornton <christopher.n.thornton@gmail.com>
2016-02-12 13:50:51 -0700

    Parse sequence file until EOF, not empty line

--- seq_qc/seq_parsers.py
+++ seq_qc/seq_parsers.py
@@ -5,37 +5,36 @@ def fastq_parser(filehandle, line=None):
     """
     if line is None:
         line = filehandle.readline()
-    line = line.strip()
     while line:
         data = {}
         if not line.startswith('@'):
             raise IOError("Bad FASTQ format: no '@' at beginning of line")
 
         try:
-            data['identifier'], data['description'] = line[1:].split(' ', 1)
+            data['identifier'], data['description'] = line[1:].strip().split(' ', 1)
         except ValueError:  # No optional annotations
-            data['identifier'] = line[1:]
+            data['identifier'] = line[1:].strip()
             data['description'] = ''
             pass
 
         # Extract the sequence lines
         sequence = []
-        line = filehandle.readline().strip()
+        line = filehandle.readline()
         while not line.startswith('+') and not line.startswith('#'):
-            sequence.append(line)
-            line = filehandle.readline().strip()
+            sequence.append(line.strip())
+            line = filehandle.readline()
 
         data['sequence'] = ''.join(sequence)
 
         # Extract the quality lines
         quality = []
-        line = filehandle.readline().strip()
+        line = filehandle.readline()
         seqlen = len(data['sequence'])
         aclen = 0
-        while not line == '' and aclen < seqlen:
-            quality.append(line)
+        while not line == '' and aclen <= seqlen:
+            quality.append(line.strip())
             aclen += len(line)
-            line = filehandle.readline().strip()
+            line = filehandle.readline()
 
         data['quality'] = ''.join(quality)
         if len(data['sequence']) != len(data['quality']):
@@ -50,26 +49,24 @@ def fasta_parser(filehandle, line=None):
     """
     if line is None:
         line = filehandle.readline()
-
     while line:
         data = {}
 
-        line = line.strip()
         if not line.startswith('>'):
             raise IOError("Bad FASTA format: no '>' at beginning of line")
 
         try:
-            data['identifier'], data['description'] = line[1:].split(' ', 1)
+            data['identifier'], data['description'] = line[1:].strip().split(' ', 1)
         except ValueError:  # No optional description
-            data['identifier'] = line[1:]
+            data['identifier'] = line[1:].strip()
             data['description'] = ''
 
         # Collect sequence lines into a list
         sequence = []
-        line = filehandle.readline().strip()
+        line = filehandle.readline()
         while line and not line.startswith('>'):
-            sequence.append(line)
-            line = filehandle.readline().strip()
+            sequence.append(line.strip())
+            line = filehandle.readline()
 
         data['sequence'] = ''.join(sequence)
         yield data
@@ -95,9 +92,9 @@ def interleaved_parser(file_iter):
     for record in file_iter:
         if prev_record:
             if pairs.verify_paired(prev_record, record):
-                yield (prev_record, record)  # it's a pair!
+                yield (prev_record, record) # it's a pair!
                 record = None
-            else:                                   # orphan.
+            else: # orphan.
                 raise pairs.UnpairedReadsError("Unpaired reads found. Data "
                     "may contain orphans or is not ordered properly", 
                     prev_record, record)
